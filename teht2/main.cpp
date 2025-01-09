aaee20a5e16d8e23dd86e3412fa0817059c36eee
jerejk03 <jere.j.kaasalainen@gmail.com>
2025-01-09 14:19:06 +0200

    viikon 1 tehtavan 2 viimeinen osa viimeistely

--- teht2/main.cpp
+++ teht2/main.cpp
@@ -23,9 +23,9 @@ int game(int maxnum)
     int arvauslkm = 0;
     srand(time(0)); //asetetaan seed aikaleimalla, mistä arvotaan luvut
     cout << "Arvaa luku valilta 1-" << maxnum << "." <<endl;
-    int satunnaisluku = 1 + (rand() % maxnum); //laitetaan arvotut luvut välille 0-19 ja lisätään siihen +1, jotta arvotut luvut ovat väliltä 1-20
+    int satunnaisluku = 1 + (rand() % maxnum); //laitetaan arvotut luvut välille 0-maxnum-1 ja lisätään siihen +1, jotta arvotut luvut ovat väliltä 1-maxnum
     int luettuluku= 0;
-    while(luettuluku != satunnaisluku){ //jatketaan while silmukkaa niin kauan, kuin arvattu luku ei ole sama kuin arvottu luku+
+    while(luettuluku != satunnaisluku){ //jatketaan while silmukkaa niin kauan, kuin arvattu luku ei ole sama kuin arvottu luku
         cin >> luettuluku;
 
         arvauslkm++;
